cf2db6446c1c27bc252277323047a245c232b933
Matthew Welch <dmw9400@gmail.com>
2017-11-16 20:51:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, value|
       holidays_caps = holiday.capitalize
       holidays_underscore = holidays_caps.split("_")
-      holidays_final = holidays_unde
+      holidays_final = holidays_underscore.split.each(&:capitalize)
       new_value = value.join(", ")
       puts "  #{holidays_final}: #{new_value}"
     end
